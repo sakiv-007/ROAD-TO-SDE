82381a086a7fe354d33834e14bc41aaafbc0fb4e
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-02 20:54:16 +0530

    repo restructured

---
+++ 01__Web-Development/02__CSS/__CSS-Projects/sushi site/js/script.js
@@ -0,0 +1,44 @@
+// import AOS from "aos";
+// // import "aos/dist/aos.css";
+// import "aos.css"
+// import "aos.js"
+
+// // // init AOS animation
+// AOS.init({
+//     duration: 1000,
+//     offset: 100,
+// });
+
+
+
+
+
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   AOS.init({
+//     duration: 1000,
+//     offset: 100,
+//   });
+
+//   setTimeout(() => {
+//     AOS.refresh();
+//   }, 500);
+// });
+
+
+
+
+
+
+      document.addEventListener("DOMContentLoaded", function () {
+        AOS.init({
+          duration: 1000,
+          offset: 100,
+        });
+
+        setTimeout(() => {
+          AOS.refresh();
+        }, 500);
+      });
