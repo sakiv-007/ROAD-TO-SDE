a14c223f08eed555283dbe0d7223be342e592445
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-10 18:41:06 +0530

    js

--- 01__Web-Development/05__JAVASCRIPT/hello.js
+++ 01__Web-Development/05__JAVASCRIPT/hello.js
@@ -1,1 +1,9 @@
 console.log("hello world!");
+console.log("hii!");
+
+process.stdout.write("heyy")
+process.stdout.write("heyy hii")
+
+
+console.table{{city: "Batala"}}
+console.warn{{city: "Batala"}}
