995c9d61aa11e728a6b844f224f94a59b2b3662a
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-10 20:55:38 +0530

    non primitives

---
+++ 01__Web-Development/05__JAVASCRIPT/part2/nonPrimitives.js
@@ -0,0 +1,37 @@
+const username = {
+    "first name": "vikas",
+    isLoggedin: true
+};
+
+username.firstname = "mr. vikas";
+username.lastname = "Mahajan";
+
+console.log(username["first name"]);
+console.log(username.lastname);
+console.log(username);
+console.log(typeof username);
+
+
+let today =  new Date();
+// console.log(today.getDate());
+
+
+//Arrays
+
+// let heroes = ["a", "b", "c" ]
+
+let anotherUser = ["vikas", true];
+// console.log(anotherUser[1]);
+
+
+let isValue = "2abc";
+
+console.log("1" + 1);
+console.log(1 + "1");
+console.log(isValue);
+console.log(Number(isValue));
+console.log(typeof(Number(isValue)));
+console.log(typeof Number(isValue));
+
+
+
---
+++ 01__Web-Development/05__JAVASCRIPT/part2/primitives.js
@@ -0,0 +1,52 @@
+//Numbers
+
+let balance = 120;
+let anotherBalance = new Number(1220);
+
+// console.log(balance);
+// console.log(anotherBalance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+
+//Boolean 
+
+let isActive = true;
+let isReallyActive = new Boolean(true) // not recommended
+
+
+//null and  undefined
+
+let firstname = null;
+let lastname = undefined;
+// console.log(firstname);
+// console.log(lastname);
+
+
+//String
+
+let myString = "hello"
+let myStringOne = 'hola'
+let username = "vikas"
+
+let oldGreet = myString + " " + "vikas";
+// console.log(oldGreet);
+
+let greetMessage = ` Hello ${username} !`;
+// console.log(greetMessage);
+
+let demo1 = `value is ${5*6}`
+// console.log(demo1);
+
+
+let sm1 = Symbol("vikas");
+let sm2 = Symbol("vikas");
+
+console.log(sm1);
+console.log(sm1 == sm2);
+
+
+
+
