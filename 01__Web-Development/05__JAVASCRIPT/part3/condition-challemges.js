7f395069a798607fdac891eb92a8cd60e630fcaf
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-12 18:29:02 +0530

    conditions

---
+++ 01__Web-Development/05__JAVASCRIPT/part3/condition-challemges.js
@@ -0,0 +1,67 @@
+//check if a no. is greater than another number 
+
+/*
+let num1 = 5
+let num2 = 9
+
+if (num1 < num2) {
+    console.log("number 1 is greater than number 2");  
+}else{
+    console.log("number 2 is greater than number 1");
+    
+}
+*/
+
+// cheking if a string is equal to another String
+
+// let username = "vikas"
+// let anotherUsername = "vikas"
+
+// if(username == anotherUsername){
+//     console.log("PICK another username");
+// }else {
+//     console.log("you can pick this username");
+    
+// }
+
+
+//checking if a number is a number or not:
+
+// let score = "45"
+
+// if (typeof score === "number") {
+//     console.log("Yes, this is a number");
+    
+// }else{
+//     console.log("no that is not a number");
+    
+// }
+    
+
+//checking if a boolean is a true or false:
+
+let isTeadReady = false;
+
+if (isTeadReady) {
+    console.log("Tea is Ready");
+    
+}else{
+   // console.log("Tea is Not Ready");
+    
+}
+
+
+
+
+// checking if an array is empty or not:
+
+let items = ["vikas"];
+console.log(items.length);
+if (items.length === 0) {
+    console.log("Array is Empty");
+    
+    
+} else {
+    console.log("Array is Not Empty");
+    
+}
