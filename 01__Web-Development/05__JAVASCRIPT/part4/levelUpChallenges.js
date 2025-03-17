df5e5441271ec1d6a66282be4d5c44057376ee3e
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-17 21:48:26 +0530

    for in loop

--- 01__Web-Development/05__JAVASCRIPT/part4/levelUpChallenges.js
+++ 01__Web-Development/05__JAVASCRIPT/part4/levelUpChallenges.js
@@ -27,15 +27,77 @@ for (let i = 0; i < teas.length; i++) {
        }
        visitedCities.push(cities[i])  
     }
- console.log(visitedCities);
+//  console.log(visitedCities);
  
 
 // 3. Use a 'for-of' loop to iterate through the  arrray '[1,2,3,4,5]' and stop when the number '4' is found. 
     // Store the numbers before '4' in an array named 'smallNumbers'.
 
+    let numbers = [1,2,3,4,5];
+    let smallNumbers = []
+
+for (const num of numbers) {
+    if (num === 4) {
+        break;
+    }
+        smallNumbers.push(num)
+    }
+// console.log(smallNumbers);
 
 
 // 4.use a 'for-of' loop to iterate thorugh the array '["chai","green tea", "herbal tea", "black tea"] and skip "herbal tea".
     // Store the other teas in an array named 'preferedTeas'.
 
+let teass = ["chai","green tea", "herbal tea", "black tea"];
+let preferedTeas = []
+
+for (const tea of teass) {
+    if (tea === "Herbal tea" || tea === "herbal tea") {
+        continue;
+    }
+    preferedTeas.push(tea)
+}
+// console.log(preferedTeas);
+
+
+/* 
+ 5. Use a 'for-in' loop to loop through an object containing city population.
+        stop the loop when the population of '"Berlin"' is found and store all previous cities population in a new object named 'cityPopulation'.
+
+        let citiesPopulation = {
+             "London" : 8900000,
+             "New York" : 8400000,
+             "Paris" : 2200000,
+             "Berlin" : 3500000
+        };
+
+  */
+
+        let citiesPopulation = {
+            "London" : 8900000,
+            "New York" : 8400000,
+            "Paris" : 2200000,
+            "Berlin" : 3500000
+       };
+
+       let cityNewPopulations = {};
+    //    console.log(Object.keys(citiesPopulation));
+    //    console.log(Object.values(citiesPopulation));
+       
+
+    for (const city in citiesPopulation) {
+                // key = value 
+                if (city == "Berlin") {
+                    break;
+                    
+                }
+
+
+            cityNewPopulations[city] = citiesPopulation[city] ;           
+        }
+    // console.log(cityNewPopulations);
+
+
+
+    
     
