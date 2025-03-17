3eb611e5bc6b9a3082dc035bcbb24245ba9cbe9a
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-17 21:58:32 +0530

    for in loop

--- 01__Web-Development/05__JAVASCRIPT/part4/levelUpChallenges.js
+++ 01__Web-Development/05__JAVASCRIPT/part4/levelUpChallenges.js
@@ -98,6 +98,33 @@ for (const tea of teass) {
     // console.log(cityNewPopulations);
 
 
+/* 
+6. Use a for-in loop to loop through ab object containing city populations.
+        skip any city with a population below 3 million and store the rest in a new object named 'largeCities'.
 
-    
-    
+        let worldCities = {
+            "Sydney" : 5000000,
+            "Tokyo" : 9000000,
+            "Berlin" : 3500000,
+            "Paris" : 2200000
+         };
+
+     */
+
+         let worldCities = {
+            "Sydney" : 5000000,
+            "Tokyo" : 9000000,
+            "Berlin" : 3500000,
+            "Paris" : 2200000
+         };
+
+         let largeCities = {}
+
+         for (const city  in worldCities) {
+                if (worldCities[city] < 3000000) {
+                    continue;
+                }
+                largeCities[city] = worldCities[city]
+            }
+         console.log(largeCities);
+         
