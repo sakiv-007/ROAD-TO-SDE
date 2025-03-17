15dfdc424bb1ac0186c3b9660e706e74d80c01ce
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-17 21:08:11 +0530

    loops

--- 01__Web-Development/05__JAVASCRIPT/part4/levelUpChallenges.js
+++ 01__Web-Development/05__JAVASCRIPT/part4/levelUpChallenges.js
@@ -10,16 +10,32 @@ for (let i = 0; i < teas.length; i++) {
     }  
     selectedTeas.push(teas[i])  
 }
-console.log(selectedTeas);
+// console.log(selectedTeas);
 
 
 
 // 2. Write a 'for' loop that loops through the array '["London", "New York", "Paris", "Berlin"] and skips '"Paris"' .
     // Store the other cities in a new array named 'visitedCities'.
 
+    let cities = ["London", "New York", "Paris", "Berlin"];
+    let visitedCities = []
+
+    for (let i = 0; i < cities.length; i++) {
+       if (cities[i] === "Paris") {
+        continue;
+        
+       }
+       visitedCities.push(cities[i])  
+    }
+ console.log(visitedCities);
  
 
 // 3. Use a 'for-of' loop to iterate through the  arrray '[1,2,3,4,5]' and stop when the number '4' is found. 
     // Store the numbers before '4' in an array named 'smallNumbers'.
+
+
+
+// 4.use a 'for-of' loop to iterate thorugh the array '["chai","green tea", "herbal tea", "black tea"] and skip "herbal tea".
+    // Store the other teas in an array named 'preferedTeas'.
 
     
