bbd47dd3b28e793ff3aad1528df375eb8d67b3fc
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-17 18:48:09 +0530

    loop

--- 01__Web-Development/05__JAVASCRIPT/part4/loopChallenges.js
+++ 01__Web-Development/05__JAVASCRIPT/part4/loopChallenges.js
@@ -6,4 +6,23 @@ while (i<=5) {
    sum += i;
    i++; 
 }
-console.log(sum);
+// console.log(sum);
+
+
+//write a while loop that counts down from 5 to 1 and stores the numbers in an array named 'countdown'
+
+let countdown = [];
+let j = 5;
+while (j>0){
+   countdown.push(j);
+   j--;
+}
+// console.log(countdown);
+
+//write a 'do while' loop that prompts a user to enter their favorite tea until they enter "stop" 
+// store each tea type in an array named 'teacollection'
+
+
+//write a do while loop that adds numbers from 1 to 3 and stores the result in a variable named total
+
+
