23b8bf13d5a405bced76c6b4940ca3c5a68a1253
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-19 19:22:07 +0530

    functions

--- 01__Web-Development/05__JAVASCRIPT/part5/functions.js
+++ 01__Web-Development/05__JAVASCRIPT/part5/functions.js
@@ -3,8 +3,23 @@
 
  */
 
+function makeTea(typeOfTea){
+    return `Making ${typeOfTea}`
+}
+let teaOrder = makeTea("lemon tea")
+// console.log(teaOrder);
+
 /* 
 2. Create a function named orderOfTea that take one parameter teatype inside this functiion crate another function named confirmOrder that returns a message like order confirmed for chai 
 Call confirmorder from within orderTea and return the result
 
  */
+function orderTea(teaType){
+    function confirmOrder(){
+        return `Order confirmed for chai`
+    }
+    return confirmOrder()
+}
+
+let orderConfirmation = orderTea("chai")
+console.log(orderConfirmation);
