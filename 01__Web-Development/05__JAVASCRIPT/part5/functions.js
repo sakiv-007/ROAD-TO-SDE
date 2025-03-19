b33e330c2195e62ee41afa821f77f9d958e6a88d
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-19 20:16:26 +0530

    functions

--- 01__Web-Development/05__JAVASCRIPT/part5/functions.js
+++ 01__Web-Development/05__JAVASCRIPT/part5/functions.js
@@ -22,4 +22,48 @@ function orderTea(teaType){
 }
 
 let orderConfirmation = orderTea("chai")
-console.log(orderConfirmation);
+// console.log(orderConfirmation);
+
+/* 
+3. Write an arrow function named calculateTotal that takes two parameters price and quantity. the function should return the total cost by multiplying the price and quantity
+Store the result in a variable named totalCost
+
+ */
+const calculateTotal = (price, quantity) => {
+    return price * quantity
+}
+let totalCost = calculateTotal(499 , 10)
+// console.log(totalCost);
+
+
+/* 
+4. Write a function named processTeaOrder that takes another function makeTea as parameter and calls it eith the argument "earl grey"
+Return the result of calling makeTea
+
+ */
+function makeTea(typeOfTea){
+    return `maketea: ${typeOfTea}`
+}
+
+function processTeaOrder(teaFunction){
+    return teaFunction("earl grey")
+}
+
+let order = processTeaOrder(makeTea)
+// console.log(order);
+
+
+/* 
+5. Write a function named createTeaMaker that returns another function the retuner function should take one parameter teaType and return a message like "making grren tea "
+
+store the returned function ina a variable named teaMaker and call it with green tea
+
+ */
+function createTeaMaker(){
+    return function (teaType){
+        return`Making ${teaType}`
+    }
+}
+
+let teaMaker = createTeaMaker();
+console.log(teaMaker("grren tea"));
