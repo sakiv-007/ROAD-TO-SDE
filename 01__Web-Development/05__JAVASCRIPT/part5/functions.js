2ac008de1fd7dcd0229e083f7eb0c81c81ecad6b
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-19 19:04:29 +0530

    added questions for functions in js

---
+++ 01__Web-Development/05__JAVASCRIPT/part5/functions.js
@@ -0,0 +1,10 @@
+/* 
+1. Write a function named 'makeTea' that takes one pearameter 'typeOfTea', and returns a string like '"Making grren tea"' when called with '"green tea". Store the result in a variable named  'teaOrder'.
+
+ */
+
+/* 
+2. Create a function named orderOfTea that take one parameter teatype inside this functiion crate another function named confirmOrder that returns a message like order confirmed for chai 
+Call confirmorder from within orderTea and return the result
+
+ */
