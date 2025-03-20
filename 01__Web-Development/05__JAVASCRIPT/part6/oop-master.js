ac2bdc4de46c4baf195c0d78cd75092b926f829b
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-20 21:41:27 +0530

    ooooopsss

--- 01__Web-Development/05__JAVASCRIPT/part6/oop-master.js
+++ 01__Web-Development/05__JAVASCRIPT/part6/oop-master.js
@@ -59,9 +59,157 @@ class Car extends vehicle {
 }
 
 let myCar =  new Car("Toyota", "Corolla")
-console.log(myCar.start());
-console.log(myCar.drive());
+// console.log(myCar.start());
+// console.log(myCar.drive());
 
 
 let vehOne =  new vehicle("Toyota", "Corolla")
-console.log(vehOne.make);
+// console.log(vehOne.make);
+
+
+
+
+
+
+
+//Encapsulation
+
+class BankAccount {
+    #balance = 0;
+
+    deposit(amount){
+        this.#balance += amount
+        return this.#balance
+    }
+    getBalance(){
+        return `$ ${this.#balance}`
+    }
+}
+
+let account = new BankAccount();
+// console.log(account.getBalance());
+
+
+
+
+
+
+
+
+
+// Abstraction 
+
+
+class CoffeeMachine {
+    start() {
+        // call DB
+        // filter value  etc etc
+
+        return `Starting the machine...`
+    }
+    brewCoffee(){
+        //complex calculations
+        return `Brewing coffee....`
+    }
+
+    pressStartButton(){
+        let msgOne = this.start()
+        let msgTwo = this.brewCoffee()
+        return `${msgOne} + ${msgTwo}`
+    }
+}
+
+let myMachine =  new CoffeeMachine()
+// console.log(myMachine.start());
+// console.log(myMachine.brewCoffee());
+
+
+// console.log(myMachine.pressStartButton());
+
+
+
+
+
+
+
+
+
+// Polymorphism
+
+class Bird {
+    fly(){
+        return `Flying...`
+    }
+}
+
+class Penguin extends Bird {
+    fly(){
+        return `Penguins can't Fly`
+    }
+}
+
+let bird =  new Bird()
+let penguin =  new Penguin()
+// console.log(bird.fly());
+// console.log(penguin.fly());
+
+
+
+
+
+// Static method
+
+class Calculator {     // static functions can only be called by the class name
+    static add(a,b)
+    {
+        return a+b;
+    }
+}
+// let miniCalc = new Calculator()
+// console.log(miniCalc.add(2,3));             this will throw the error
+
+
+// console.log(Calculator.add(2,3));
+
+
+
+
+
+
+
+
+// Getters and Setters
+
+class Employee {
+    #salary;
+    constructor(name, salary)
+    {
+        if (salary < 0) {
+            throw new Error(`"Salary can't be negative"`);
+            
+        }
+        this.name =  name
+        this.#salary = salary
+    }
+
+    get salary()
+    {
+        return `You are not allowed to see the Salary`
+    }
+
+    set salary(value)
+    {
+        if (value< 0) {
+            {
+                console.error("Invalid Salary");
+                
+            }
+            
+        }else {
+            this._salary = value;
+        }
+    }
+}
+let emp = new Employee("Alice", -50000)
+console.log(emp._salary);
+emp.salary = 60000;
