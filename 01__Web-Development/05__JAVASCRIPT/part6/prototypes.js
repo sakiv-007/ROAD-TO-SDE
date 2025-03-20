78b0e5cc6ab1e0b215c6f6cd4aeb1f67a45039c1
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-03-20 19:29:59 +0530

    proto

--- 01__Web-Development/05__JAVASCRIPT/part6/prototypes.js
+++ 01__Web-Development/05__JAVASCRIPT/part6/prototypes.js
@@ -5,6 +5,19 @@ let lenovo = {
 };
 let tomHardware = {}
 
-console.log(`lenovo`, lenovo.__proto__);
+// console.log(`lenovo`, lenovo.__proto__);
+
+
+let genericCar = {tyres: 4}
+let tesla = {
+    driver: "AI"
+}
+
+Object.setPrototypeOf(tesla, genericCar)
+
+// console.log(`tesla`, tesla.tyres);
+
+console.log(`tesla`, Object.getPrototypeOf(tesla));
+
 
 
