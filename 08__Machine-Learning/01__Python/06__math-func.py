e105f766d64aa1b8c989d76b5dfb64d88030a488
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-18 18:08:45 +0530

    Update math function script and add exercise for calculating hypotenuse

--- 08__Machine-Learning/01__Python/06__math-func.py
+++ 08__Machine-Learning/01__Python/06__math-func.py
@@ -1,10 +1,12 @@
 import math
 
-x = 9
+x = 9.1
 
 
 #print(math.pi)
 #print(math.e)
-result = math.sqrt(x)
+#result = math.sqrt(x)
+#result = math.ceil(x)
+result = math.floor(x) 
 
 print(result)
---
+++ 08__Machine-Learning/01__Python/07__Exerceise.py
@@ -0,0 +1,27 @@
+# calc circumference of a circle
+
+import math
+
+'''
+radius = float(input("Enter the radius of a circle: "))
+
+circumference = 2 * math.pi * radius
+print(f"The radius of circle is: {round(circumference, 2)}cm")
+
+
+# Area of circle
+
+area = math.pi * pow(radius, 2)
+
+print(f"Area of the cicle is {round(area, 2)}cm²")
+
+'''
+
+# calc the hypotenuse of triangle
+
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f"The length of hypotenuse is: {c} cm")
