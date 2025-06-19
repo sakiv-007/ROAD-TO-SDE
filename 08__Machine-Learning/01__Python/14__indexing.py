6d9085e00ace547dda49fb167d74063523019bbf
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-19 14:44:54 +0530

    Enhance string indexing example with additional explanations and reverse number program

--- 08__Machine-Learning/01__Python/14__indexing.py
+++ 08__Machine-Learning/01__Python/14__indexing.py
@@ -1,4 +1,26 @@
-# indexing - accessing elements of a sequence using [] (indexing operator) 
+# string indexing - accessing elements of a sequence using [] (indexing operator) 
 
-# [start : end : step]
+# [start : end : step]   start is inclusive end is exclusive
 
+credit_number = "1234 5678-9655"
+
+print(credit_number[4])
+
+print(credit_number[0:4]) # also
+print(credit_number[:4])  # does the same
+print(credit_number[5:])  # returns from index 5 to the end of the list
+print(credit_number[-4])  # starts from the end side
+print(credit_number[::2])  # step:  returns every second number and skips the between
+
+
+
+#### Program to find the last four digits of the credit card
+
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+
+#### Program to reverse numbers
+
+credit_number = credit_number[::-1]
+print(credit_number)
