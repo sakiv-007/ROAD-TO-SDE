9e094a57edab8f26dc35b2377b561ba99d207e0d
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-20 16:55:28 +0530

    Refactor countdown timer to display hours, minutes, and seconds during countdown

--- 08__Machine-Learning/01__Python/python__project/countdown-timer.py
+++ 08__Machine-Learning/01__Python/python__project/countdown-timer.py
@@ -3,8 +3,11 @@ import time
 my_time = int(input("Enter the time in seconds"))
 
 # for x in reversed(range(0,my_time)):
-for x in reversed(range(my_time, 0, -1)):
-    print(x)
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{hours:02}:{seconds:02}")
     time.sleep(1)
 
 print("Time's Up")
