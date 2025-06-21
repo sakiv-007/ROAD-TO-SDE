2af5f8bee900bad163e1c70dca27a2ac4f6b3fc2
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-21 11:43:41 +0530

    Refactor nested loops example to accept user-defined rows, columns, and symbol for printing

--- 08__Machine-Learning/01__Python/18__nested-loops.py
+++ 08__Machine-Learning/01__Python/18__nested-loops.py
@@ -3,8 +3,13 @@
 # for x in range(1, 10):
 #     print(x, end=" ")
 
-for x in range(3):
-    
-    for y in range(1, 10):
-        print(y, end="")
+rows = int(input("Enter the # of rows: "))
+cols = int(input("Enter the 3 of columns: "))
+symbol = input("Enter the Symbol to use: ")
+
+
+for x in range(rows):
+
+    for y in range(cols):
+        print(symbol, end=" ")
     print()
