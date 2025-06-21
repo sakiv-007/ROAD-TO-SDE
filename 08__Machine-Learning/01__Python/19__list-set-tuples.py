950c0e92382377134315993944c7ee8ed3f2b83b
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-21 12:14:30 +0530

    Add list and tuple examples with fruit manipulation operations

---
+++ 08__Machine-Learning/01__Python/19__list-set-tuples.py
@@ -0,0 +1,39 @@
+# Collection = single "variable" used to store multiple values
+
+# List = [] ordered and changeable, duplicates are allowed
+
+# Set = {} unodered and immutable, but Add/Remove OK, NO duplicates
+
+# Tuples = () ordered and unchangeable, Duplicates OK, FASTER
+
+fruits = ["apple", "banana", "orange", "coconut"]
+
+# print(fruits[::-1])
+
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# fruits[0] = "pineapple"
+
+fruits.append("pineapple")
+fruits.remove("apple")
+fruits.insert(0, "litchi")
+fruits.sort()
+fruits.reverse()
+fruits.clear()
+fruits.index("apple")
+
+
+
+
+
+print(fruits)
+
+# for fruit in fruits:
+#     print(fruit)
