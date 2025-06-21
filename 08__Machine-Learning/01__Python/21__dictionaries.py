749e50bb8c75cadfac5f121ef9ecc6188779206d
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-21 15:24:44 +0530

    Add dictionary example with capital city updates and iteration

---
+++ 08__Machine-Learning/01__Python/21__dictionaries.py
@@ -0,0 +1,49 @@
+# dictionary =  a dictionary of {key:value} pairs 
+#   ordered and changeable. No duplicates
+
+capitals = {"USA": "WAshington DC",
+            "India": "New Delhi",
+            "China": "Beijing",
+             "Russia" : "Moscow" }
+
+# print(dir(capitals))
+# print(help(capitals))
+
+# print(capitals.get("USA"))
+# print(capitals)
+
+
+# to check if a particular key exist in the list or not
+
+# if capitals.get("Russia"):
+#     print("That capital Exists")
+# else:
+#     print("that capital does not exist")
+
+
+capitals.update({"Germany" : "Berlin"})
+capitals.update({"USA" : "Detroit"})
+# capitals.pop("China")  # removes china from list
+# capitals.popitem()  # removes the latest item
+
+# capitals.clear()
+
+keys = capitals.keys()  # to get only the keys in the dictionary not the values
+
+# print(keys)
+
+values = capitals.values()
+# print(values)
+
+
+# for key in capitals.keys():
+    # print(key)
+ 
+# print(capitals)
+
+
+# items = capitals.items()
+# print(items)
+
+for key, value in capitals.items():
+    print(f"{key} : {value}")
