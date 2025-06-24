ccad2f9468b89140c7b2e3752909feb50aa34f5e
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-24 13:33:47 +0530

    Add number guessing game implementation with input validation and feedback

---
+++ 08__Machine-Learning/01__Python/python__project/07__number-guessing.py
@@ -0,0 +1,39 @@
+import random
+
+lowest_num = 1
+highest_num = 5
+
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
+
+print("Python Number Guessing Game")
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+
+while is_running:
+    guess = input("Enter your Guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"Please select a number between {lowest_num} and {highest_num} : ")
+
+        elif guess < answer:
+            print("Too low! Try again!")
+        elif guess > answer:
+            print("Too high! Try again!")
+        else:
+            print(f"CORRECT! The asnwer was {answer}")
+            print(f"number of guesses: ")
+            is_running = False
+
+    else:
+        print("Invalid Guess")
+        print(f"Please select a number between {lowest_num} and {highest_num} : ")
+
+
+
