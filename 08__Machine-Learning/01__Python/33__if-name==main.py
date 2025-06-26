ae9fb2ea25d0f5bf38c2e354619c690683b8cc37
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-06-26 12:42:40 +0530

    Add implementation of a banking program with deposit, withdraw, and balance display functionalities

---
+++ 08__Machine-Learning/01__Python/33__if-name==main.py
@@ -0,0 +1,18 @@
+# if __name__ == main__ = (this script can be imported or run standalone) 
+
+#          functions and classes in his modlule can reused 
+
+#          without the main block of code executing
+
+
+'''
+def main():
+    # Your program goes here
+
+if __name__ == '__main__':
+    main()
+
+
+ '''
+
+ # couldn't get this topic tbh
