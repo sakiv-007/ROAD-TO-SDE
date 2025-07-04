7dfa4c3c8c8d835207aa7bfb0ca71e209ee352ac
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-07-04 12:35:15 +0530

    latex-resume

---
+++ 08__Machine-Learning/01__Python/44__property.py
@@ -0,0 +1,37 @@
+# Property =  Decorator used to define a method as a property (it can be accessed like an attribute) 
+#            Benefit: Add additional logic when read, write, or delete attributes 
+#            gives you getter, setter and deleter method
+
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
+
+
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
+    
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+        else:
+            print("Width must be greater than zero")
+
+    
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be greater than zero")
+
+rectangle = Rectangle(3,4)
+
+print(rectangle.width)
+print(rectangle.height)
